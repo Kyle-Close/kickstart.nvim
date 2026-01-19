e557062330af95bf8909fd4dfbd665cfac8df071
Kyle-Close <k.james.close@gmail.com>
2026-01-19 09:54:33 -0800

    added keymap to go to previous buffer

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,18 +1,26 @@
 -- Exit insert mode with jj instead of escape
 vim.keymap.set('i', 'jj', '<Esc>')
+
 -- '0' goes to first character in line
 vim.keymap.set('n', '0', '^')
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
 -- Exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
 -- Window navigation
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Go back to previous buffer
+vim.keymap.set('n', '<leader>bb', '<C-^>', { desc = 'Switch to previous buffer' })
 
 -- Buffer delete keymap with inline function
 vim.keymap.set('n', '<leader>bd', function()
