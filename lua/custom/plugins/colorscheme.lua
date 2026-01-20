b74e3b5cc2011df42e7ad461500dab0a6485851d
Kyle-Close <k.james.close@gmail.com>
2026-01-20 08:58:26 -0800

    changed color scheme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,8 +1,11 @@
+--[[ 
+
 return { -- You can easily change to a different colorscheme.
   -- Change the name of the colorscheme plugin below, and then
   -- change the command in the config to whatever the name of that colorscheme is.
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --[[
   'folke/tokyonight.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
@@ -19,3 +22,17 @@ return { -- You can easily change to a different colorscheme.
     vim.cmd.colorscheme 'tokyonight-night'
   end,
 }
+
+--]]
+
+return {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- Optionally configure and load the colorscheme
+      -- directly inside the plugin declaration.
+      vim.g.everforest_enable_italic = true
+      vim.cmd.colorscheme('everforest')
+    end
+  }
